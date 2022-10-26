056d18bf8058925f471d9ed36478d1f74160f36c
dgnasci <110121995+dgnasci@users.noreply.github.com>
2022-10-26 00:30:20 -0300

    att

---
+++ urna 2/Js/java.js
@@ -0,0 +1,18 @@
+let nulo = 0
+let votos = 0
+let lula = 0
+let mito = 0
+let votacao = vote.value
+mito.style.visibility = "hidden"
+fazl.style.visibility = "hidden"
+function urna(f){
+
+    if(f == 1){
+        nulo++
+        votos++
+        alert("Seu voto foi computado")
+    }
+    else if(f == 2){
+        if ()
+    }
+}
