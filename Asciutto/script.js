332e9cbdd67b027ae681b7c9c760593b424cecc1
dgnasci <110121995+dgnasci@users.noreply.github.com>
2023-10-18 21:14:29 -0300

    Add files via upload

--- Asciutto/script.js
+++ Asciutto/script.js
@@ -1,42 +1,68 @@
-// Função para exibir campos de acordo com a forma de pagamento selecionada
 document.addEventListener("DOMContentLoaded", function() {
-    const cartaoCampos = document.getElementById("cartao-campos");
-    const boletoQRCodeBotoes = document.getElementById("boleto-qrcode-botoes");
-    const valorTotalModal = document.getElementById("valor-total-modal");
+    const cartaoTab = document.getElementById("cartao-tab");
+    const boletoTab = document.getElementById("boleto-tab");
+    const qrcodeTab = document.getElementById("qrcode-tab");
 
-    const cartaoRadio = document.getElementById("cartao");
-    const boletoRadio = document.getElementById("boleto");
-    const qrcodeRadio = document.getElementById("qrcode");
+    const cartaoTabContent = document.getElementById("cartao");
+    const boletoTabContent = document.getElementById("boleto");
+    const qrcodeTabContent = document.getElementById("qrcode");
 
-    cartaoRadio.addEventListener("change", function() {
-        cartaoCampos.style.display = "block";
-        boletoQRCodeBotoes.style.display = "none";
-        valorTotalModal.textContent = "Total: R$ 49.99";
-    });
+    // Função para exibir a aba de cartão
+    function exibirCartaoTab() {
+        cartaoTabContent.style.display = "block";
+        boletoTabContent.style.display = "none";
+        qrcodeTabContent.style.display = "none";
+    }
 
-    boletoRadio.addEventListener("change", function() {
-        cartaoCampos.style.display = "none";
-        boletoQRCodeBotoes.style.display = "block";
-        valorTotalModal.textContent = "Total: R$ 49.99";
-    });
+    // Função para exibir a aba de boleto
+    function exibirBoletoTab() {
+        cartaoTabContent.style.display = "none";
+        boletoTabContent.style.display = "block";
+        qrcodeTabContent.style.display = "none";
+    }
 
-    qrcodeRadio.addEventListener("change", function() {
-        cartaoCampos.style.display = "none";
-        boletoQRCodeBotoes.style.display = "block";
-        valorTotalModal.textContent = "Total: R$ 49.99";
-    });
+    // Função para exibir a aba de QR Code
+    function exibirQRCodeTab() {
+        cartaoTabContent.style.display = "none";
+        boletoTabContent.style.display = "none";
+        qrcodeTabContent.style.display = "block";
+    }
+
+    // Ações ao selecionar a forma de pagamento
+    cartaoTab.addEventListener("click", exibirCartaoTab);
+    boletoTab.addEventListener("click", exibirBoletoTab);
+    qrcodeTab.addEventListener("click", exibirQRCodeTab);
 
     // Lógica para o botão "Gerar Boleto"
     const gerarBoletoBtn = document.getElementById("gerarBoletoBtn");
     gerarBoletoBtn.addEventListener("click", function() {
-        // Implemente aqui a lógica para gerar o boleto
+        // Lógica para o botão "Gerar Boleto"
+    const gerarBoletoBtn = document.getElementById("gerarBoletoBtn");
+    gerarBoletoBtn.addEventListener("click", function() {
+        // Oculta o QR Code, se estiver visível
+        document.getElementById("qrcodeImage").style.display = "none";
+
+        // Exibe o boleto
+        const boletoImage = document.getElementById("boletoImage");
+        boletoImage.src = "boleto.png"; // Substitua pelo URL da imagem do boleto
+        boletoImage.style.display = "block";
+    });
         alert("Boleto gerado com sucesso!");
     });
 
     // Lógica para o botão "Gerar QR Code"
     const gerarQRCodeBtn = document.getElementById("gerarQRCodeBtn");
     gerarQRCodeBtn.addEventListener("click", function() {
-        // Implemente aqui a lógica para gerar o QR Code
+       // Lógica para o botão "Gerar QR Code"
+    const gerarQRCodeBtn = document.getElementById("gerarQRCodeBtn");
+    gerarQRCodeBtn.addEventListener("click", function() {
+        // Oculta o boleto, se estiver visível
+        document.getElementById("boletoImage").style.display = "none";
+
+        // Exibe o QR Code
+        const qrcodeImage = document.getElementById("qrcodeImage");
+        qrcodeImage.src = "link_para_qrcode.png"; // Substitua pelo URL da imagem do QR Code
+        qrcodeImage.style.display = "block";
         alert("QR Code gerado com sucesso!");
     });
 
@@ -46,4 +72,5 @@ document.addEventListener("DOMContentLoaded", function() {
         // Implemente aqui a lógica para finalizar a compra
         alert("Compra finalizada com sucesso");
     });
+   
 });
