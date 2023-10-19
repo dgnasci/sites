9df9558e7ea27676fbb30f4a22243cfe6e685919
dgnasci <110121995+dgnasci@users.noreply.github.com>
2023-10-19 00:22:12 -0300

    Add files via upload

--- Asciutto/script.js
+++ Asciutto/script.js
@@ -1,43 +1,82 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const cartaoTab = document.getElementById("cartao-tab");
+    const boletoTab = document.getElementById("boleto-tab");
+    const qrcodeTab = document.getElementById("qrcode-tab");
+
+    const cartaoTabContent = document.getElementById("cartao");
+    const boletoTabContent = document.getElementById("boleto");
+    const qrcodeTabContent = document.getElementById("qrcode");
+
+    // Função para exibir a aba de cartão
+    function exibirCartaoTab() {
+        cartaoTabContent.style.display = "block";
+        boletoTabContent.style.display = "none";
+        qrcodeTabContent.style display = "none";
+    }
+
+    // Função para exibir a aba de boleto
+    function exibirBoletoTab() {
+        cartaoTabContent.style.display = "none";
+        boletoTabContent.style.display = "block";
+        qrcodeTabContent.style.display = "none";
+    }
+
+    // Função para exibir a aba de QR Code
+    function exibirQRCodeTab() {
+        cartaoTabContent.style.display = "none";
+        boletoTabContent.style.display = "none";
+        qrcodeTabContent.style.display = "block";
+    }
+
+    // Ações ao selecionar a forma de pagamento
+    cartaoTab.addEventListener("click", exibirCartaoTab);
+    boletoTab.addEventListener("click", exibirBoletoTab);
+    qrcodeTab.addEventListener("click", exibirQRCodeTab);
+
     // Lógica para o botão "Gerar Boleto"
     const gerarBoletoBtn = document.getElementById("gerarBoletoBtn");
     gerarBoletoBtn.addEventListener("click", function() {
-        // Troca a imagem do boleto e exibe a frase
+          // Lógica para o botão "Gerar Boleto"
+    const gerarBoletoBtn = document.getElementById("gerarBoletoBtn");
+    gerarBoletoBtn.addEventListener("click", function() {
+        // Troca a imagem do boleto
         const boletoImage = document.getElementById("boletoImage");
         boletoImage.src = "boleto.jpg";
+
+        // Oculta a frase
         const boletoPhrase = document.getElementById("boletoPhrase");
-        boletoPhrase.textContent = "Vencimento em 1 dia útil. A data de entrega pode ser alterada devido ao tempo de processamento do Boleto.";
+        boletoPhrase.style.display = "none";
+
+        // Oculta o botão de gerar boleto
+        gerarBoletoBtn.style.display = "none";
+    
+        alert("Boleto gerado com sucesso!");
     });
 
     // Lógica para o botão "Gerar QR Code"
     const gerarQRCodeBtn = document.getElementById("gerarQRCodeBtn");
     gerarQRCodeBtn.addEventListener("click", function() {
-        // Troca a imagem do QR code e exibe a frase
+   // Lógica para o botão "Gerar QR Code"
+    const gerarQRCodeBtn = document.getElementById("gerarQRCodeBtn");
+    gerarQRCodeBtn.addEventListener("click", function() {
+        // Troca a imagem do QR code
         const qrcodeImage = document.getElementById("qrcodeImage");
         qrcodeImage.src = "qrcode.jpg";
-        const qrcodePhrase = document.getElementById("qrcodePhrase");
-        qrcodePhrase.textContent = "O código Pix gerado para o pagamento é válido por 30 minutos após a finalização do pedido.";
-    });
 
-    // Lógica para mostrar/ocultar a aba de pagamento em Cartão
-    const cartaoTab = document.getElementById("cartao-tab");
-    const cartaoContent = document.getElementById("cartao");
-    cartaoTab.addEventListener("click", function() {
-        // Certifique-se de que o conteúdo da aba de pagamento em Cartão seja mostrado ao clicar na aba
-        if (!cartaoContent.classList.contains("show")) {
-            cartaoContent.classList.add("show");
-        }
+        // Oculta a frase
+        const qrcodePhrase = document.getElementById("qrcodePhrase");
+        qrcodePhrase.style.display = "none";
+
+        // Oculta o botão de gerar QR Code
+        gerarQRCodeBtn.style.display = "none";
+  
+        alert("QR Code gerado com sucesso!");
     });
 
     // Lógica para o botão "Finalizar Compra"
     const finalizarCompraBtn = document.getElementById("finalizarCompraBtn");
     finalizarCompraBtn.addEventListener("click", function() {
-        // Substitua a lógica para finalizar a compra aqui
-
-        // Exibe um alerta
+        // Implemente aqui a lógica para finalizar a compra
         alert("Compra finalizada com sucesso");
-
-        // Redireciona de volta para o índice
-        window.location.href = "index.html";
     });
 });
